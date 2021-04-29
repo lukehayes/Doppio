42ccaa027366e3c706dd63ba0a60b0deb16c94fd
lukehayes <lukepurplehayes@gmail.com>
2021-04-29 17:50:53 +0100

    Added index.php for testing purposes.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Doppio</title>
+    <link rel="stylesheet" href="doppio.css" title="" type="">
+</head>
+<body>
+
+<div class="doppio-grid">
+<?php for($i = 0; $i <= 11; $i++): ?>
+    <div class="col">
+        <article>
+
+            <h3>Crypto Currency.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<button type="">Wealth Is Coming</button>
+
+        </article>
+    </div> <!-- col -->
+
+<?php endfor; ?>
+</div>
+
+</body>
+</html>
