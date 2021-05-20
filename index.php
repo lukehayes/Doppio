e595e7af1128f0761e672666a96b20d939f577c8
lukehayes <lukepurplehayes@gmail.com>
2021-05-20 17:45:56 +0100

    Added meta viewport tag to index.php

--- index.php
+++ index.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <title>Doppio</title>
     <link rel="stylesheet" href="doppio.css" title="" type="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
-<div class="doppio-grid">
+<div class="grid">
 <?php for($i = 0; $i <= 11; $i++): ?>
-    <div class="col">
-        <article>
+    <article>
 
-            <h3>Crypto Currency.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-<button type="">Wealth Is Coming</button>
+        <h3>Crypto Currency.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<button type="">Wealth</button>
+<button class="dp-btn-dark">Wealth</button>
 
-        </article>
-    </div> <!-- col -->
+    </article>
 
 <?php endfor; ?>
 </div>
